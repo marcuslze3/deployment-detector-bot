1c0ede2fe218d242a3305d4f138b8f83ca6e4827
unknown <mzl20@ic.ac.uk>
2022-07-25 17:23:45 +0800

    refac: removed unecessary txEvent check

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -9,88 +9,70 @@ import {
 import agent, {
   NETHERMIND_DEPLOYER_ADDRESS,
   FORTA_CONTRACT_ADDRESS,
-  BOT_DEPLOY_EVENT,
+  BOT_DEPLOY_FUNC,
 } from "./agent";
+
+import { TestTransactionEvent } from "forta-agent-tools/lib/tests";
 
 describe("nethermind new agent deployment bot", () => {
   let handleTransaction: HandleTransaction;
-  const mockTxEvent = createTransactionEvent({} as any);
+  //const mockTxEvent = createTransactionEvent({} as any);
 
   beforeAll(() => {
     handleTransaction = agent.handleTransaction;
   });
 
   describe("handleTransaction", () => {
-
     // first test
     it("returns empty findings if no agent deployed", async () => {
+      const mockTxEvent = new TestTransactionEvent();
       mockTxEvent.filterFunction = jest.fn().mockReturnValue([]);
-
       const findings = await handleTransaction(mockTxEvent);
 
       // expect empty return since no agent deployed
       expect(findings).toStrictEqual([]);
       expect(mockTxEvent.filterFunction).toHaveBeenCalledTimes(1);
       expect(mockTxEvent.filterFunction).toHaveBeenCalledWith(
-        BOT_DEPLOY_EVENT,
+        BOT_DEPLOY_FUNC,
         FORTA_CONTRACT_ADDRESS
       );
     });
 
     // second test
     it("returns empty findings if agent deployed but not by Nethermind", async () => {
-      const mockTetherTransferEvent = {
-        args: {
-          agentId:  1,
-          owner: "0xabc",
-        },
-      };
-      mockTxEvent.filterLog = jest
-        .fn()
-        .mockReturnValue([mockTetherTransferEvent]);
-
+      const mockTxEvent = new TestTransactionEvent()
+        .setFrom("0xabc")
+        .setTo(FORTA_CONTRACT_ADDRESS);
+      mockTxEvent.filterFunction = jest.fn().mockReturnValue([]);
       const findings = await handleTransaction(mockTxEvent);
 
       expect(findings).toStrictEqual([]);
       expect(mockTxEvent.filterFunction).toHaveBeenCalledTimes(1);
       expect(mockTxEvent.filterFunction).toHaveBeenCalledWith(
-        BOT_DEPLOY_EVENT,
+        BOT_DEPLOY_FUNC,
         FORTA_CONTRACT_ADDRESS
       );
     });
 
     // third test
     it("returns findings if agent deployed by Nethermind", async () => {
-      const mockTetherTransferEvent = {
-        args: {
-          agentId:  1,
-          owner: NETHERMIND_DEPLOYER_ADDRESS,
-        },
-      };
-      mockTxEvent.filterLog = jest
-        .fn()
-        .mockReturnValue([mockTetherTransferEvent]);
+      const mockTxEvent = new TestTransactionEvent()
+        .setFrom(NETHERMIND_DEPLOYER_ADDRESS)
+        .setTo(FORTA_CONTRACT_ADDRESS);
 
       const findings = await handleTransaction(mockTxEvent);
 
-      // expect empty return since no Tether transferred
+      // expect findings since Agent deployed by Nethermind
       expect(findings).toStrictEqual([
         Finding.fromObject({
-          name: "Nethermind Deployed Bot",
-          description: "Nethermind has just deployed a Forta Bot",
+          name: "Nethermind Deployed Forta Agent",
+          description: "Nethermind has just deployed a Forta Agent",
           alertId: "DEPLOY-1",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
+          metadata: {},
         }),
       ]);
-      expect(mockTxEvent.filterFunction).toHaveBeenCalledTimes(1);
-      expect(mockTxEvent.filterFunction).toHaveBeenCalledWith(
-        BOT_DEPLOY_EVENT,
-        FORTA_CONTRACT_ADDRESS
-      );
     });
-
   });
 });
-
-
--- src/agent.ts
+++ src/agent.ts
@@ -8,45 +8,52 @@ import {
   FindingType,
 } from "forta-agent";
 
-
-export const NETHERMIND_DEPLOYER_ADDRESS = '0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8';
-export const BOT_DEPLOY_EVENT = "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
-export const FORTA_CONTRACT_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 let findingsCount = 0;
+
+export const NETHERMIND_DEPLOYER_ADDRESS =
+  "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
+export const BOT_DEPLOY_FUNC =
+  "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
+export const FORTA_CONTRACT_ADDRESS =
+  "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   const findings: Finding[] = [];
 
-  // limiting this agent to emit only 5 findings so thast the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
-  
   // filter the transaction logs for the Nethermind deployer create agent function
   // returns an array of transaction logs that have called the createAgent function on the Forta contract addresss
   const nethermindDeployAgentCalls = txEvent.filterFunction(
-    BOT_DEPLOY_EVENT,
+    BOT_DEPLOY_FUNC,
     FORTA_CONTRACT_ADDRESS
   );
 
+  console.log("here");
   nethermindDeployAgentCalls.forEach((deployCalls) => {
     // extract deploy bot event arguments
     const { agentId, owner, metadata, chainIds } = deployCalls.args;
     // if the owner is nethermind deployer, report it
-    if (owner == NETHERMIND_DEPLOYER_ADDRESS) {
+    console.log(deployCalls.args);
+    if (
+      txEvent.from == NETHERMIND_DEPLOYER_ADDRESS.toLowerCase() &&
+      txEvent.to == FORTA_CONTRACT_ADDRESS.toLowerCase()
+    ) {
+      console.log("detected!");
+
       findings.push(
         Finding.fromObject({
-          name: "Nethermind Deployed Bot",
-          description: "Nethermind has just deployed a Forta Bot",
-          alertId: "DEPLOY-1",
-          severity: FindingSeverity.Low,  
-          type: FindingType.Info
+          name: "Nethermind Deployed Forta Agent",
+          description: "Nethermind has just deployed a Forta Agent",
+          alertId: "DEPLOY-" + findingsCount.toString(),
+          severity: FindingSeverity.Low,
+          type: FindingType.Info,
+          metadata: {},
         })
       );
-
-      findingsCount++;
     }
 
+    findingsCount += 1;
   });
 
   return findings;
